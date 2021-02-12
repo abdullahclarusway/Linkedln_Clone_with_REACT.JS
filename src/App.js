87973165e1d57feb67eaec7e478a9309765d89f0
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-02-12 23:10:46 +0100

    Initialize Project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Let's built LinkedIn Clone</h1>
+
+      {/* Header */}
+      {/* App Body */}
+      {/* Sidebar */}
+      {/* Feed */}
+      {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
