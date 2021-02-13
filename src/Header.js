2dcac29e552b3dfd76d68ae49ac20f14ae30cb7e
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-02-13 01:01:00 +0100

    Add headerOption component

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 const Header = () => {
   return (
@@ -15,7 +21,17 @@ const Header = () => {
           <input type="text" />
         </div>
       </div>
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOption title="Home" Icon={HomeIcon} />
+        <HeaderOption title="My Network" Icon={SupervisorAccountIcon} />
+        <HeaderOption title="Jobs" Icon={BusinessCenterIcon} />
+        <HeaderOption title="Messaging" Icon={ChatIcon} />
+        <HeaderOption title="Notifications" Icon={NotificationsIcon} />
+        <HeaderOption
+          title="me"
+          avatar="https://thumbs.dreamstime.com/b/happy-smiling-geek-hipster-beard-man-cool-avatar-geek-man-avatar-104871313.jpg"
+        />
+      </div>
     </div>
   );
 };
