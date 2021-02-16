317031296cbcc1b46b129bffc18ef9e78022f0c1
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-02-16 23:57:47 +0100

    Add login component to app

---
+++ src/Login.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { useState } from "react";
+import { auth } from "./firebase";
+import "./Login.css";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+  const register = () => {};
+  const loginToApp = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="login">
+      <img
+        src="https://plantafruit.files.wordpress.com/2013/06/linkedin-button.jpg"
+        alt="linkedin-icon"
+      />
+      <form>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Full name (required if registering)"
+          type="text"
+        />
+        <input
+          value={profilePic}
+          onChange={(e) => setProfilePic(e.target.value)}
+          placeholder="Profile pic URL (optional)"
+          type="text"
+        />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          type="email"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          type="password"
+        />
+        <button type="submit" onClick={loginToApp}>
+          Sign In
+        </button>
+      </form>
+      <p>
+        Not a member?
+        <span className="login__register" onClick={register}>
+          Register Now
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
